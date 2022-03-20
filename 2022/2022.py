9d41605db33e9b1d1eff58acdb1a8ad90376ab51
Jack Lowry <jack.lowry@mail.utoronto.ca>
2022-03-20 15:22:00 -0400

    #2022 success

--- 2022/2022.py
+++ 2022/2022.py
@@ -1,3 +1,21 @@
 def construct2DArray(original, m, n):
 
-    
+    output = []
+
+    if len(original) != m * n:
+        return output
+
+    index = 0
+
+    for row in range(m):
+        output.append([])
+        for col in range(n):
+            output[row].append(original[index])
+            index += 1
+
+    return output
+
+
+print(construct2DArray([1, 2, 3, 4], 2, 2))
+print(construct2DArray([1, 2, 3], 1, 3))
+print(construct2DArray([1, 2], 1, 1))
