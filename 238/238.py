9eaeb878cc68d7241d15165013c9e302e94132bc
Jack Lowry <jack.lowry@mail.utoronto.ca>
2022-03-27 07:16:00 -0400

    #238 success

--- 238/238.py
+++ 238/238.py
@@ -1,8 +1,21 @@
 def solution(nums):
+    p = 1
+    n = len(nums)
     output = []
-    for i in range(len(nums)):
-        
-        
+    print(output, p)
+    print("first loop")
+    for i in range(0, n):
+        output.append(p)
+        p = p * nums[i]
+        print(output, p)
+    p = 1
+    print("second loop")
+    for i in range(n - 1, -1, -1):
+        output[i] = output[i] * p
+        p = p * nums[i]
+        print(output, p)
+    return output
 
-print(solution([1,2,3,4]))
-print(solution([-1,1,0,-3,3]))
+
+print("SOLUTION: ", solution([1, 2, 3, 4]))
+print("SOLUTION: ", solution([-1, 1, 0, -3, 3]))
