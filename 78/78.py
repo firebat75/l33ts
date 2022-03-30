0a18442b2cdbe0da0a513a97dd8993b58bbcecd4
jack <--unset>
2022-03-30 08:46:28 -0400

    #78 success

--- 78/78.py
+++ 78/78.py
@@ -1,1 +1,16 @@
 def solution(nums):
+    def backtracking(temp=[], i=0):
+        if temp not in output:
+            output.append(temp)
+
+        if i < len(nums):
+            backtracking(temp + [nums[i]], i + 1)
+            backtracking(temp, i + 1)
+
+    output = []
+    backtracking()
+    return output
+
+
+print(solution([1, 2, 3]))
+print(solution([0]))
