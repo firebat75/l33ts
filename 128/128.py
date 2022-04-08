f7afc4f1f64aec582a07863bc23134d34e0bdc34
Jack Lowry <jacklowry1337@gmail.com>
2022-04-08 03:24:36 -0400

    #128 started

---
+++ 128/128.py
@@ -0,0 +1,7 @@
+def solution(nums):
+
+    return None
+
+
+print(solution([100, 4, 200, 1, 3, 2]))
+print(solution([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]))
--- 46/46.py
+++ 46/46.py
@@ -7,7 +7,7 @@ def solution(nums):
     def dfs(nums, path, res):
         if not nums:
             res.append(path)
-            # return # backtracking
+
         for i in range(len(nums)):
             dfs(nums[:i] + nums[i + 1 :], path + [nums[i]], res)
 
