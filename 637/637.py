37c7ca37d20d373ee2c473c3f250a5fd61dc9dd0
Jack Lowry <jacklowry1337@gmail.com>
2022-04-08 23:40:50 -0400

    #637 success

--- 637/637.py
+++ 637/637.py
@@ -5,6 +5,23 @@ class TreeNode:
         self.right = right
 
 
-def averageOfLevels(root):
-    if not root.right and not root.left:  # if node is a leaf
-        return [root.val]  # return node value
+def solution(root):
+    q, output = [root], []
+    while len(q):
+        qlen, row = len(q), 0
+        for i in range(qlen):
+            curr = q.pop(0)
+            row += curr.val
+            if curr.left:
+                q.append(curr.left)
+            if curr.right:
+                q.append(curr.right)
+
+        output.append(row / qlen)
+    return output
+
+
+a = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+print(solution(a))
+b = TreeNode(3, TreeNode(9, TreeNode(15), TreeNode(7)), TreeNode(7))
+print(solution(b))
