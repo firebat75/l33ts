dee5f61ad87c7cd2cc8772210f9bd28f947985d6
firebat75 <jacklowry1337@gmail.com>
2022-06-03 04:37:16 -0400

    #304 TLE on case 18/24

--- 304/304.py
+++ 304/304.py
@@ -4,11 +4,11 @@ class NumMatrix:
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         output = 0
-        for y in range(row1, row2 + 1):
-            for x in range(col1, col2 + 1):
-                output += self.matrix[y][x]
-
         # for y in range(row1, row2 + 1):
-        #     output += sum(self.matrix[y][col1 : col2 + 1])
+        #     for x in range(col1, col2 + 1):
+        #         output += self.matrix[y][x]
+
+        for y in range(row1, row2 + 1):
+            output += sum(self.matrix[y][col1 : col2 + 1])
 
         return output
