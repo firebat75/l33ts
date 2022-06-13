2ab82cdc29b3a1ab76dbd3591a3de5970e37b897
firebat75 <jacklowry1337@gmail.com>
2022-06-13 16:52:26 -0400

    #14 success

---
+++ 14/14.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 0: 
+            return ""
+        s1, s2 = min(strs), max(strs)
+        i = 0
+        while i < len(s1) and i < len(s2) and s1[i] == s2[i]: 
+            i += 1
+        return s1[:i]
